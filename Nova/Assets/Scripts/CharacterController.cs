0a7e1723a11058ff66388839e37ae5e8f4ee2ae9
Andrew Fisher <Andrew Fisher>
2017-01-08 15:14:13 -0800

    Started on example platformer

---
+++ Nova/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+    private Rigidbody2D rb2d;
+    private float jumpForce = 150f;
+    private float moveForce = 2f;
+    private bool jumping = false;
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb2d.AddForce(Vector3.up * jumpForce);
+        }
+        /*if (Input.GetKeyDown(KeyCode.D))
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Vector3 position = this.transform.position;
+            position.x--;
+            this.transform.position = position;
+        }*/
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * moveForce * Time.deltaTime;
+
+    }
+}
