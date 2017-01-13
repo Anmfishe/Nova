ac8415c80c07aaecf171669c86564a0ebb74bbfe
Andrew Maxwell Fisher <anmfishe@crm.ucsc.edu>
2017-01-13 14:36:52 -0800

    Nova sprite in, camera script in, will try to fix camera script

--- Nova/Assets/Scripts/CharacterController.cs
+++ Nova/Assets/Scripts/CharacterController.cs
@@ -2,39 +2,95 @@
 using System.Collections;
 
 public class CharacterController : MonoBehaviour {
-    private Rigidbody2D rb2d;
-    private float jumpForce = 150f;
-    private float moveForce = 2f;
-    private bool jumping = false;
-	// Use this for initialization
-	void Start () {
-        rb2d = GetComponent<Rigidbody2D>();
-	}
-	
+    private bool facingRight = true; // What direction is Nova facing
+
+    [SerializeField]
+    private float maxSpeed = 10f; // Maximum speed Nova can move
+    [SerializeField]
+    private float jumpForce = 400f; // Jump force
+    [SerializeField]
+    private bool airControl = false; // Can the player influence jumping?
+    [SerializeField]
+    private LayerMask whatIsGround; // A mask determining what is ground to the character
+    private Transform groundCheck; // A position marking where to check if the player is grounded.
+    private float groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    private bool grounded = false; // Whether or not the player is grounded.
+    private Transform ceilingCheck; // A position marking where to check for ceilings
+    private float ceilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
+    //private Animator anim; // Reference to the player's animator component.
+
+    void Awake()
+    {
+        //set up all references
+        groundCheck = transform.Find("GroundCheck");
+        ceilingCheck = transform.Find("CeilingCheck");
+        //anim = GetComponent<Animator>();
+    }
+
+    
 	// Update is called once per frame
 	void Update () {
 	
 	}
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            rb2d.AddForce(Vector3.up * jumpForce);
-        }
-        /*if (Input.GetKeyDown(KeyCode.D))
-        {
-            Vector3 position = this.transform.position;
-            position.x++;
-            this.transform.position = position;
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Vector3 position = this.transform.position;
-            position.x--;
-            this.transform.position = position;
-        }*/
-        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        transform.position += move * moveForce * Time.deltaTime;
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
+        //anim.SetBool("Ground", grounded);
 
+    }
+    public void Move(float move, bool crouch, bool jump)
+    {
+
+
+        // If crouching, check to see if the character can stand up
+        /*if (!crouch && anim.GetBool("Crouch"))
+        {
+            // If the character has a ceiling preventing them from standing up, keep them crouching
+            if (Physics2D.OverlapCircle(ceilingCheck.position, ceilingRadius, whatIsGround))
+                crouch = true;
+        }
+
+        // Set whether or not the character is crouching in the animator
+        anim.SetBool("Crouch", crouch);*/
+
+        //only control the player if grounded or airControl is turned on
+        if (grounded || airControl)
+        {
+            // Reduce the speed if crouching by the crouchSpeed multiplier
+           // move = (crouch ? move * crouchSpeed : move);
+
+            // The Speed animator parameter is set to the absolute value of the horizontal input.
+            //anim.SetFloat("Speed", Mathf.Abs(move));
+
+            // Move the character
+            GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+            // If the input is moving the player right and the player is facing left...
+            if (move > 0 && !facingRight)
+                // ... flip the player.
+                Flip();
+            // Otherwise if the input is moving the player left and the player is facing right...
+            else if (move < 0 && facingRight)
+                // ... flip the player.
+                Flip();
+        }
+        // If the player should jump...
+        if (grounded && jump)//&& anim.GetBool("Ground")
+        {
+            // Add a vertical force to the player.
+            grounded = false;
+            //anim.SetBool("Ground", false);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
