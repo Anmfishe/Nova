504b723424ca121df0f5e585a360dd056df14336
Andrew Maxwell Fisher <anmfishe@crm.ucsc.edu>
2017-01-29 12:25:42 -0800

    Started working on climbing

---
+++ Nova/Assets/ClimbableArea.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbableArea : MonoBehaviour {
+    private CharacterController playerController;
+	// Use this for initialization
+	void Start () {
+        playerController = FindObjectOfType<CharacterController>();
+	}
+	
+	void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.name == "Nova-pieces")
+        {
+            playerController.canClimb = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.name == "Nova-pieces")
+        {
+            playerController.canClimb = false;
+        }
+    }
+}
