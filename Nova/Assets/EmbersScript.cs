44e9a49df58df999f6e149c826f1257425418f19
Andrew Maxwell Fisher <anmfishe@crm.ucsc.edu>
2017-02-19 20:02:51 -0800

    Early version of fire in, will work on killing Nova soon

---
+++ Nova/Assets/EmbersScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmbersScript : MonoBehaviour {
+    private SpriteRenderer sr;
+    private float rate;
+    private float one = 1;
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        rate = Random.Range(0.005f, 0.01f);
+        rate *= -1;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        one += rate;
+        if(one <= 0 || one >= 1)
+        {
+            rate *= -1;
+        }
+        float currVal = Mathf.Lerp(1, 0.25f, one);
+        //sr.color = new Color(currVal, currVal, currVal, 1f);
+        sr.color = new Color(1f, 1f, 1f, currVal);
+	}
+}
