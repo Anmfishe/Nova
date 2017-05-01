6681d51c6b21fa6be6d26c9d27c201f95aae7797
Andrew Fisher <anmfishe@ucsc.edu>
2017-04-30 22:57:26 -0700

    Started level 2

--- Nova/Assets/Scripts/ClimbableArea.cs
+++ Nova/Assets/Scripts/ClimbableArea.cs
@@ -3,27 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ClimbableArea : MonoBehaviour {
+    private GameObject player;
     private CharacterController playerController;
     private int numColliders;
     public int collidersRequiredToStop = 0;
     public bool requiresRight = false;
 	// Use this for initialization
 	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
         playerController = FindObjectOfType<CharacterController>();
 	}
-	
+    private void Update()
+    {
+        Debug.Log(transform.position.x + " " + Time.time);
+    }
+    
 	void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-            if (requiresRight && playerController.getDir() || !requiresRight)
+            numColliders++;
+            if (requiresRight && playerController.getDir()  || !requiresRight)
             {
-                numColliders++;
+                
                 playerController.canClimb = true;
             }
         }
     }
-    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            if(requiresRight && !(player.transform.position.x < (transform.position.x - 0.25)))
+            {
+                playerController.canClimb = false;
+            }
+            
+        }
+    }
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == "Player")
