da87aa47de37f7a6bc5d12fded97645ddd69c145
Andrew Maxwell Fisher <anmfishe@crm.ucsc.edu>
2017-07-10 17:02:02 -0700

    Elder final in progress

---
+++ Nova/Assets/Scripts/One Shot Controller/ElderTreeEndingv2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElderTreeEndingv2 : MonoBehaviour {
+    private CharacterController cc;
+    private FireNovaController fnc;
+    private Camera cam;
+	// Use this for initialization
+	void Start () {
+        cc = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>();
+        fnc = GameObject.FindGameObjectWithTag("FireLady").GetComponent<FireNovaController>();
+        cam = Camera.main;
+        StartCoroutine(Opening());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    IEnumerator Opening()
+    {
+        cc.canMove = false;
+        cc.Flip();
+        cam.GetComponent<UnitySampleAssets._2D.Camera2DFollow>().startFadeIn();
+        cc.anim.Play("NovaSitToStand");
+        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1f);
+        cc.canMove = true;
+
+    }
+}
