7fed99fb4c7e3cd2961a8a253ea01dbe8d649b42
sspatil13 <sspatil@ucsc.edu>
2017-08-01 18:18:30 -0700

    Basic animation, camera settings done.
    Fixed burnable, brambles. Basic animation, camera settings done.

---
+++ Nova/Assets/FireNovaStopper.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireNovaStopper : MonoBehaviour {
+
+    private CharacterController cc;
+    bool second = true;
+
+    // Use this for initialization
+    void Start () {
+        cc = GameObject.FindGameObjectWithTag("FireLady").GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /*
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "FireLady")
+        {
+            second = false;
+            StartCoroutine(FireStopping());
+        }
+    }*/
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("FireLady") && !GetComponent<EdgeCollider2D>().isTrigger)
+        {
+            GetComponent<EdgeCollider2D>().isTrigger = false;
+        }
+    }
+
+   /* IEnumerator FireStopping()
+    {
+        cc.canMove = false;
+        yield return new WaitForSeconds(0.4f);
+        cc.anim.SetBool("FireNovaLevel3Idle", true);
+       /* yield return new WaitForSeconds(2f);
+        cc.canMove = true;
+        cc.anim.SetBool("FireNovaLevel3Idle", false);
+    }*/
+}
