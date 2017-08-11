d98380dcb99592cbd609ad2c7f30fc48fb9e92e8
kabakel <kate.barr.kelly@gmail.com>
2017-08-11 16:45:34 -0700

    Added SFX and Music to Level3
    I added level3 music, 2D sound.
    I changed branch breaking SFX and added collision to the falling pieces.
    I added rain in three areas so there wouldn't be rain in the underground section.
    I added altar's idle loop.
    I added SFX to the 2 waterfalls.
    I added rain on water to the Pond.
    I moved the listener from Nova and created a SmartAudioListener.  This should lead to better sounding stereo sounds for the user.
    All added sound were set to 3D.

---
+++ Nova/Assets/Scripts/kabakel/AudioListenerPlacement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioListenerPlacement : MonoBehaviour {
+
+    public GameObject CameraObj;
+    public GameObject PlayerObj;
+
+    [Range(0,1)]
+    public float PlayerToCameraPercentX;
+
+    [Range(0,1)]
+    public float PlayerToCameraPercentY;
+
+    [Range(0,1)]
+    public float PlayerToCameraPercentZ;
+
+	// Update is called once per frame
+    void LateUpdate () {
+        Vector3 playerPos = PlayerObj.transform.position;
+        Vector3 cameraPos = CameraObj.transform.position;
+        Vector3 desiredPos;
+        desiredPos.x = Mathf.Lerp(playerPos.x, cameraPos.x, PlayerToCameraPercentX);
+        desiredPos.y = Mathf.Lerp(playerPos.y, cameraPos.y, PlayerToCameraPercentY);
+        desiredPos.z = Mathf.Lerp(playerPos.z, cameraPos.z, PlayerToCameraPercentZ);
+        transform.position = desiredPos;
+	}
+}
